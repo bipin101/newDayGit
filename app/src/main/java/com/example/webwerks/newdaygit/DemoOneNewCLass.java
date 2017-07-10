be1a575bd369647cc1cfa0201b6bdfc20745334f
bipin101 <bipin.dubey@neosofttech.com>
2017-07-10 14:32:03 +0530

    demo commit 2nd

---
+++ app/src/main/java/com/example/webwerks/newdaygit/DemoOneNewCLass.java
@@ -0,0 +1,14 @@
+package com.example.webwerks.newdaygit;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by webwerks on 10/7/17.
+ */
+
+public class DemoOneNewCLass extends AppCompatActivity {
+
+
+    public DemoOneNewCLass() {kjsgfgit
+    }
+}
