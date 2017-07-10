7b6feeacb11899b04576c6f90afc097e358cb05c
bipin101 <bipin.dubey@neosofttech.com>
2017-07-10 13:49:44 +0530

    After brach creation first commit

---
+++ app/src/main/java/com/example/webwerks/newdaygit/DemoActivity.java
@@ -0,0 +1,10 @@
+package com.example.webwerks.newdaygit;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by webwerks on 10/7/17.
+ */
+
+public class DemoActivity extends AppCompatActivity {
+}
